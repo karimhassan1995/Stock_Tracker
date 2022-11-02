6688a318e30bfb021844664fc7b1c5072a0efced
karimhassan1995 <karimelmorsy95@gmail.com>
2022-11-02 20:19:48 +0200

    add frienships controller and frienship model

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,19 @@
+class FriendshipsController < ApplicationController
+  def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "following user"
+    else
+      flash[:alert] = "there was something wrong wih the racking request"
+    end
+    redirect_to friends_path
+  end
+
+  def destroy
+    friendship = current_user.friendships.where(friend_id: params[:id]).first
+    friendship.destroy
+    flash[:notice] = "stopped following"
+    redirect_to friends_path
+  end
+end
