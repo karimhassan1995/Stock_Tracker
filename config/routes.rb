25b992b49a9e8d5d12c356b0f776d8f38862254a
karimhassan1995 <karimelmorsy95@gmail.com>
2022-12-19 02:00:34 +0200

    adding final version

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
 
   
-  devise_for :users
+ 
+  resources :user_stocks, only: [:create , :destroy]
+    devise_for :users 
     root 'welcome#index'
     get 'my_portfolio' , to: 'users#my_portfolio'
+    get 'friends' , to: 'users#my_friends'
+    get 'search_stock' , to: 'stocks#search'
+    get 'search_friend' , to: 'users#search'
+    resources :friendships, only: [:create , :destroy]
+    resources :users, only: [:show]
    
 end
